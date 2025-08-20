c83cceb914667154c42a983095f1ed6a72174cc0
Denise Case <denisecase@gmail.com>
2025-08-19 22:30:13 -0500

    Update generated types

--- src/ci/transparency/types/__init__.py
+++ src/ci/transparency/types/__init__.py
@@ -1,18 +1,5 @@
 from .series import Series
+from .series import Interval
 from .provenance_tag import ProvenanceTag
-
-# Re-export the version written by setuptools_scm
 from ._version import __version__  # noqa: F401
-
-__all__ = ["Series", "ProvenanceTag"]
-__all__ = ["Series", "ProvenanceTag"]
-__all__ = ["Series", "ProvenanceTag"]
-__all__ = ["Series", "ProvenanceTag"]
-__all__ = ["Series", "ProvenanceTag"]
-__all__ = ["Series", "ProvenanceTag"]
-__all__ = ["Series", "ProvenanceTag"]
-__all__ = ["Series", "ProvenanceTag"]
-__all__ = ["Series", "ProvenanceTag"]
-__all__ = ["Series", "ProvenanceTag"]
-__all__ = ["Series", "ProvenanceTag"]
-__all__ = ["Series", "ProvenanceTag"]
+__all__ = ['Series', 'Interval', 'ProvenanceTag']
